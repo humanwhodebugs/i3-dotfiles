9c332532990acfae0f86c3e35779995e02040782
Muhammad Rifai <humanwhodebugs@gmail.com>
2025-03-15 00:09:26 +0700

    Added Tmux Config and MPC
    - Added my tmux config, still need to clone TPM.
    - Added mpc and tweaked my ncmpcpp to display Now Playing songs
    notification.

--- Install/install-script.sh
+++ Install/install-script.sh
@@ -28,7 +28,7 @@ echo "5. Installing Screenshot and Screenrecord Dependencies..."
 install_packages "Screenshot Dependencies" maim xclip viewnior gpu-screen-recorder
 
 echo "6. Installing MPD and NCMPCPP..."
-install_packages "MPD and NCMPCPP" mpd ncmpcpp
+install_packages "MPD and NCMPCPP" mpd ncmpcpp mpc
 
 echo "Installation process completed successfully!"
 
