5b34389f18c5a7cbbabd01dedbe6fe8ce6c4bfa0
Muhammad Rifai <humanwhodebugs@gmail.com>
2025-04-02 04:29:19 +0700

    Small Tweaks
    - Added Documentation.txt since I'm a fucking dumbass.
    - Tweaked tmux.
    - Cleaned up after-install script.
    - Good bye.

--- Install/after-install.sh
+++ Install/after-install.sh
@@ -10,14 +10,6 @@ install_packages() {
   echo "-----------------------------------"
 }
 
-# Function for installing dependencies
-install_dependencies() {
-  echo "Installing Dependencies for: $1"
-  paru -S --noconfirm --needed --asdeps "${@:2}"
-  echo "Done installing Dependencies for: $1"
-  echo "-----------------------------------"
-}
-
 # Function to clone a git repository if it doesn't exist
 clone_repo() {
   local name=$1
@@ -42,16 +34,10 @@ clone_repo() {
 echo "1. Installing Development Tools..."
 install_packages "Development Tools" neovim lazygit httpie tmux
 
-echo "2. Installing Gaming Tools..."
-install_packages "Gaming Tools (Main)" lutris lib32-mangohud mangohud
-
-echo "3. Installing Gaming Tools (Dependencies)..."
-install_dependencies "Gaming Tools (Dependencies)" giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2-compat lib32-sdl2-compat
-
-echo "4. Cloning TPM (Tmux Plugin Manager)..."
+echo "2. Cloning TPM (Tmux Plugin Manager)..."
 clone_repo "TPM" "https://github.com/tmux-plugins/tpm" "$HOME/.tmux/plugins/tpm"
 
-echo "5. Cloning Catppuccin Theme for Tmux (v2.1.2)..."
+echo "3. Cloning Catppuccin Theme for Tmux (v2.1.2)..."
 clone_repo "Catppuccin Tmux Theme" "https://github.com/catppuccin/tmux.git" "$HOME/.tmux/plugins/catppuccin" "v2.1.2"
 
 echo "All packages, dependencies, and plugins installed successfully!"
